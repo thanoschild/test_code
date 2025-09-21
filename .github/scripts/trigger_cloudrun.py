d5d8358599eeba191db0107f7bbdb5969264da7c
Sumeet Haldar <sumeethaldar731@gmail.com>
2025-09-21 17:45:05 +0530

    added workflow

--- .github/scripts/trigger_cloudrun.py
+++ .github/scripts/trigger_cloudrun.py
@@ -7,14 +7,14 @@ def main():
         "issue_number": os.environ["ISSUE_NUMBER"],
         "title": os.environ["ISSUE_TITLE"],
         "body": os.environ.get("ISSUE_BODY", ""),
-        "repo_name": os.environ["REPO_NAME"],
+        "repo": os.environ["REPO_NAME"],
     }
 
     cloud_run_url = os.environ["CLOUD_RUN_URL"]
     print("cloud run url: " + cloud_run_url)
     github_issue_endpoint = urljoin(cloud_run_url.rstrip("/") + "/", "api/v1/issues/github")
 
-    response = requests.post("https://smart-pr-service-195937916746.us-west1.run.app/api/v1/issues/github", json=payload)
+    response = requests.post(github_issue_endpoint, json=payload)
     print("Cloud Run response:", response.status_code, response.text)
     response.raise_for_status()
 
