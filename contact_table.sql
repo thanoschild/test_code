0b47c8311e8dfd5c91094faa100c67088cb44855
Sumeet Haldar <sumeethaldar731@gmail.com>
2025-09-22 16:39:33 +0530

    update code to test file

---
+++ contact_table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS `project_name.contact_raw.contact_table` (
+  `contact_id` STRING,
+  `first_name` STRING,
+  `last_name` STRING,
+  `email` STRING,
+  `phone_number` STRING,
+  `location` STRING,
+  `age` INT64,
+  `gender` STRING,
+  `date_of_birth` DATE,
+  `is_active` BOOL,
+  `company` STRING,
+  `job_title` STRING,
+  `source` STRING,
+  `purpose` STRING,
+  `created_at` TIMESTAMP,
+  `updated_at` TIMESTAMP
+);
